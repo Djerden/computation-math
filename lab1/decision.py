e0e56ffc688a579e48ab2e778a608131ec8a294a
Djeno <djeno00@mail.ru>
2024-04-02 16:40:03 +0300

    fix lab1

--- lab1/decision.py
+++ lab1/decision.py
@@ -61,8 +61,14 @@ def absolute_deviations_check(last_iter, current_iter, eps):
     values = []
     for i in range(len(last_iter)):
         values.append(abs(current_iter[i] - last_iter[i]))
-    if max(values) < eps:
+    max_value = max(values)
+    print('Критерий по абсоютным отклонениям')
+    if max_value < eps:
+        print(f'max = {max_value} < {eps}')
+        print('Заканчиваем вычисления...')
         return True
+    else: 
+        print(f'max = {max_value} > {eps}')
     return False
 
 # Метод простых итераций 
@@ -78,31 +84,52 @@ def simple_iterations(c, d, eps):
                 x_i += c[i][j]*last_d[j]
             x_i += d[i]
             current_d[i] = x_i
-
+        print(f'Итерация #{count_iter}\nПриближение: ')
+        print(current_d)
         if absolute_deviations_check(last_d, current_d, eps):
             break 
         last_d = current_d.copy()
     return current_d, count_iter 
 
+def print_matrix_and_vector(matrix, vector):
+    print('Матрица:')
+    print('[ ', end='')
+    print(matrix[0]) 
+    for i in range(1, len(matrix)):
+        if i == len(matrix)-1:
+            print(' ', matrix[i], ']')
+        else: 
+            print(' ', matrix[i])
+
+    print('Вектор:')
+    print(vector)
+
 # Функция обьединяющая все шаги для вывода полноценного решения
 def solver(matrix, vector, eps):
-    print(matrix, vector)
+    print('Получена матрица и вектор: ')
+    print_matrix_and_vector(matrix, vector)
+    
     print('Проверка матрицы на диагональное преобладание')
     if not check_diagonal_dominance(matrix):
+        print('Матрица не соответствует условию диагонального преобладания. Попытаемся привести...')
         matrix, vector = bring_to_diagonal_dominance(matrix, vector)
         if matrix == None:
             print('Матрицу невозможно привести к диагональному преобладанию')
             return
         print('Матрица приведена к диагональному преобладанию')
-        print(matrix, vector) 
+        print_matrix_and_vector(matrix, vector) 
+    else: 
+        print('Матрица соблюдает условие диагонального преобладания')
     
-    matrix, vector = get_x(matrix, vector)
     print('Получаем коэффициенты')
-    print(matrix, vector)
+    matrix, vector = get_x(matrix, vector)
+    print_matrix_and_vector(matrix, vector)
+
     if not norm_of_transformation(matrix):
         print('||C|| => 1, не удовлетворяет условию нормы преобразования')
     else: 
         print('||C|| < 1, удовлетворяет условию нормы преобразования')
+
     answer, iterations = simple_iterations(matrix, vector, eps)
     print('Ответ: ', answer)
     print('Количество итераций: ', iterations)
