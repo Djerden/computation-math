67a55444263b76ab9300ab3beba1ce3e129b517f
Djeno <djeno00@mail.ru>
2024-09-25 00:45:05 +0300

    lab 6 frontend Input Solution

--- labs3-6-gui/frontend/src/components/Lab6Components/Input.jsx
+++ labs3-6-gui/frontend/src/components/Lab6Components/Input.jsx
@@ -1,66 +1,162 @@
-export default function Input() {
+import {useState} from "react";
+
+export default function Input({requestFunction}) {
+
+    const [inputs, setInputs] = useState({
+        y0: '',
+        x0: '',
+        xn: '',
+        h: '',
+        eps: '',
+        function: "y' = x^2 - 2y"
+    });
+
+    function changeInputs(key, value) {
+        if (!(key in inputs)) {
+            return;
+        }
+        setInputs((prevState) => ({
+            ...prevState,
+            [key]: value
+        }));
+    }
+
+    function isValidInputs(data) {
+        const { y0, x0, xn, h, eps } = data;
+
+        // Проверяем, что все поля (кроме function) являются числами или null
+        if (y0 === null || x0 === null || xn === null || h === null || eps === null) {
+            alert('Все поля должны быть заполнены.');
+            return false;
+        }
+
+        // Проверяем, что все поля (кроме function) являются числами
+        if (isNaN(y0) || isNaN(x0) || isNaN(xn) || isNaN(h) || isNaN(eps)) {
+            alert('Все поля должны быть числовыми значениями, кроме поля function.');
+            return false;
+        }
+
+        // Проверяем условия
+        if (eps < 0 || eps > 1) {
+            alert('Значение eps должно быть в диапазоне от 0 до 1.');
+            return false;
+        }
+
+        if (h <= 0) {
+            alert('Значение h должно быть положительным.');
+            return false;
+        }
+
+        if (x0 >= xn) {
+            alert('Значение x0 должно быть меньше значения xn.');
+            return false;
+        }
+
+        return true;
+    }
+
+    function solveClick() {
+
+        const data = {
+            y0: inputs.y0.trim() ? Number(inputs.y0.replace(',', '.')) : null,
+            x0: inputs.x0.trim() ? Number(inputs.x0.replace(',', '.')) : null,
+            xn: inputs.xn.trim() ? Number(inputs.xn.replace(',', '.')) : null,
+            h: inputs.h.trim() ? Number(inputs.h.replace(',', '.')) : null,
+            eps: inputs.eps.trim() ? Number(inputs.eps.replace(',', '.')) : null,
+            function: inputs.function // Оставляем как строку
+        };
+        console.log(data)
+
+        if (!isValidInputs(data)) {
+            console.log('Некорректный ввод')
+            return;
+        }
+
+        requestFunction(data)
+    }
+
     return (
         <div className="flex flex-col">
             <div className="flex flex-col bg-white rounded p-6 gap-4">
-                <div className="flex flex-col gap-1">
+                <div className="flex flex-col gap-1 min-w-[300px]">
                     <h1 className="text-2xl font-bold">Input</h1>
-                    <h2>Enter the initial condition:</h2>
+                    <h2><b>Enter the initial condition:</b></h2>
                     <input
                         className={`border border-black rounded px-2 py-1`}
                         type="text"
                         placeholder="y0"
-                        value=""
-                        onChange={() => {
+                        value={inputs.y0}
+                        onChange={(e) => {
+                            changeInputs('y0', e.target.value)
                         }}
                     />
-                    <h2>Enter the differentiation interval:</h2>
+                    <h2><b>Enter the differentiation interval:</b></h2>
                     <input
                         className={`border border-black rounded px-2 py-1`}
                         type="text"
                         placeholder="x0"
-                        value=""
-                        onChange={() => {
+                        value={inputs.x0}
+                        onChange={(e) => {
+                            changeInputs('x0', e.target.value)
                         }}
                     />
                     <input
                         className={`border border-black rounded px-2 py-1`}
                         type="text"
                         placeholder="xn"
-                        value=""
-                        onChange={() => {
+                        value={inputs.xn}
+                        onChange={(e) => {
+                            changeInputs('xn', e.target.value)
                         }}
                     />
-                    <h2>Enter the step value:</h2>
+                    <h2><b>Enter the step value:</b></h2>
                     <input
                         className={`border border-black rounded px-2 py-1`}
                         type="text"
                         placeholder="h"
-                        value=""
-                        onChange={() => {
+                        value={inputs.h}
+                        onChange={(e) => {
+                            changeInputs('h', e.target.value)
                         }}
                     />
-                    <h2>Enter the accuracy:</h2>
+                    <h2><b>Enter the accuracy:</b></h2>
                     <input
                         className={`border border-black rounded px-2 py-1`}
                         type="text"
                         placeholder="eps"
-                        value=""
-                        onChange={() => {
+                        value={inputs.eps}
+                        onChange={(e) => {
+                            changeInputs('eps', e.target.value)
                         }}
                     />
                 </div>
                 <div>
-                    <h2>Select a function</h2>
+                    <h2><b>Select a function:</b></h2>
                     <p className="space-x-2">
-                        <input type="radio" name="equation" value="y' = x^2 - 2y" defaultChecked onChange={() => {}}/>
+                        <input
+                            type="radio"
+                            name="equation"
+                            value="y' = x^2 - 2y"
+                            checked={inputs.function === "y' = x^2 - 2y"}
+                            onChange={(e) => {changeInputs('function', e.target.value)}}/>
                         <label>y' = x^2 - 2y</label>
                     </p>
                     <p className="space-x-2">
-                    <input type="radio" name="equation" value="y' = y + (1+x) * y^2" onChange={() => {}}/>
+                        <input
+                            type="radio"
+                            name="equation"
+                            value="y' = y + (1+x) * y^2"
+                            checked={inputs.function === "y' = y + (1+x) * y^2"}
+                            onChange={(e) => {changeInputs('function', e.target.value)}}/>
                         <label>y' = y + (1+x) * y^2</label>
                     </p>
                     <p className="space-x-2">
-                        <input type="radio" name="equation" value="y' = 2x - 3y" onChange={() => {}}/>
+                        <input
+                            type="radio"
+                            name="equation"
+                            value="y' = 2x - 3y"
+                            checked={inputs.function === "y' = 2x - 3y"}
+                            onChange={(e) => {changeInputs('function', e.target.value)}}/>
                         <label>y' = 2x - 3y</label>
                     </p>
                 </div>
@@ -69,8 +165,7 @@ export default function Input() {
                 <div className="flex flex-row items-center bg-custom-644e5b p-6 text-white rounded-b">
                     <button
                         className="bg-black px-6 w-full rounded hover:bg-neutral-700"
-                        onClick={() => {
-                        }}
+                        onClick={solveClick}
                     >Solve
                     </button>
                 </div>
--- labs3-6-gui/frontend/src/pages/Lab6.jsx
+++ labs3-6-gui/frontend/src/pages/Lab6.jsx
@@ -1,14 +1,52 @@
+import {useState} from "react";
+
 import MainTextComponent from "../components/common/MainTextComponent.jsx";
 import Input from "../components/Lab6Components/Input.jsx";
 import Solution from "../components/Lab6Components/Solution.jsx";
 
+
 export default function Lab6() {
+
+    const [answer, setAnswer] = useState({
+        result: 'answer will be here'
+    });
+
+    function sendRequest(inputs) {
+
+        fetch('http://localhost:8000/lab6', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(inputs)
+        }).then(response => {
+            if (!response.ok) {
+                return response.json().then(errorData => {
+                    throw new Error(errorData.details || 'Произошла ошибка');
+                });
+            }
+            return response.json()
+        }).then(data => {
+                console.log('Полученные данные:')
+                console.log(data);
+
+                // обработать результат
+                setAnswer(() => ({result: data}));
+            })
+            .catch(error => {
+                console.error('Error:', error);
+                alert(`Ошибка: ${error.message}`);
+            });
+
+        console.log('Данные отправлены');
+    }
+
     return (
         <>
             <MainTextComponent labName="Numerical solution of differential equations">Lab #6</MainTextComponent>
             <div className="flex flex-row gap-6 mx-6">
-                <Input/>
-                <Solution/>
+                <Input requestFunction={sendRequest}/>
+                <Solution answer={answer}/>
             </div>
             <div>
 
